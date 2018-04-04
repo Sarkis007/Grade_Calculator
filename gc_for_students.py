822f2bb5890c234ec0109a2073f1bed5aae8e225
Sarkis007 <38066798+Sarkis007@users.noreply.github.com>
2018-04-04 19:25:41 +0400

    Add files via upload

---
+++ gc_for_students.py
@@ -0,0 +1,85 @@
+import json
+
+def loadgradesbreakdown():
+    with open('gc_setup.json') as data_file:
+        course = json.load(data_file)
+    grade_breakdown = course["course_setup"]["grade_breakdown"]
+    return grade_breakdown
+
+def loadconvmatrix():
+    with open('gc_setup.json') as data_file1:
+        conv_matrix = json.load(data_file1)["course_setup"]["conv_matrix"]
+    return conv_matrix
+
+def loadgradesfile():
+    with open('students_grades.json') as data_file1:
+        student_grade = json.load(data_file1)
+    return student_grade
+
+def askforID():
+    ID = str(raw_input("Enter student ID"))
+    return ID
+
+def checkifIDexist(student_grades, ID):
+    for key in student_grades:
+        if ID == key:
+            return 1
+    return 0
+
+def insertthegrades(gradesbreakdown, studentID):
+    newID = {studentID:{}}
+    for key in gradesbreakdown:
+        print "The percentage for " + key + "  is  " + str((gradesbreakdown[key])) + "%"
+        newID[studentID][key] = input("What is your Current Grade for " + key + " Please insert -1 if you don't have a grade yet")
+    return newID
+
+def changethegrades(studentID, student_grades):
+    for key in student_grades[studentID]:
+        print "your grade for " + str(key) + " is " + str(student_grades[studentID][key])
+        x = str(raw_input("Do you want to change your grade type y for yes, n for no"))
+        if x == "y":
+            student_grades[studentID][key] = input("What is your Current Grade for: " + key)
+    return student_grades
+
+def insertorcheck(checkifIDexist, ID, gradesbreakdown, student_grades):
+        if checkifIDexist == True:
+            x = changethegrades(ID, student_grades)
+            return x
+        elif checkifIDexist == False:
+            y = insertthegrades(gradesbreakdown, ID)
+            return y
+
+def saveGrades(student_grades, current_grades, ID):
+    student_grades[ID] = current_grades[ID]
+    print "Your grades are: " + (json.dumps(student_grades[ID]))
+    file = open("students_grades.json", "w")
+    file.write(json.dumps(student_grades))
+    file.close()
+
+def finalgrade(gradesbreakdown, current_grades, studentID):
+    finalgrade = 0
+    for key in current_grades[studentID]:
+        if current_grades[studentID][key] != -1:
+            calc_grade = int(current_grades[studentID][key]) * gradesbreakdown[key] / 100
+            finalgrade = finalgrade + calc_grade
+    return finalgrade
+
+def printgradeasletter(convmatrix, Current_Grade):
+    for x in range(len(convmatrix)):
+        if int(convmatrix[x]["max"]) >= int(Current_Grade) and int(convmatrix[x]["min"]) <= int(Current_Grade):
+
+            print "your grade is " + str(Current_Grade) + " your mark is " + str(convmatrix[x]["mark"])
+
+def main():
+
+    ID = askforID()
+    gradesbreakdown = loadgradesbreakdown()
+    convmatrix = loadconvmatrix()
+    student_grades =loadgradesfile()
+    checkifexist = checkifIDexist(student_grades, ID)
+    current_grades = insertorcheck(checkifexist, ID, gradesbreakdown, student_grades)
+    saveGrades(student_grades, current_grades, ID)
+    fingrade = finalgrade(gradesbreakdown, current_grades, ID)
+    printgradeasletter(convmatrix, fingrade)
+
+main()
